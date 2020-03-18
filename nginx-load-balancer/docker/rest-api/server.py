3ec7ebf8f67a8101c36745a84e4f073542c1be4c
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-03-18 19:31:46 +0000

    Close the file first before attempting to apply

--- nginx-load-balancer/docker/rest-api/server.py
+++ nginx-load-balancer/docker/rest-api/server.py
@@ -50,6 +50,7 @@ spec:
                     data = data.replace('"#backend_hosts_value', ','+server_host+'"#backend_hosts_value')
             try:
                 file.write(data)
+                file.close()
                 arguments = ['/opt/bin/getResponse.sh','%s' % name]
                 self.wfile.write(arguments)
                 p = Popen(arguments, stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1)
