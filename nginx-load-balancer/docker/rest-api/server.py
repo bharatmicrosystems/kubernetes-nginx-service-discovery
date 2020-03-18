2e49b98fa95a038e10ce40ba995cfeabfc48722b
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-03-18 16:35:30 +0000

    Updated server.py

--- nginx-load-balancer/docker/rest-api/server.py
+++ nginx-load-balancer/docker/rest-api/server.py
@@ -15,7 +15,8 @@ class S(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def _setup_ingress(name, host, server_host, port):
+    def _setup_ingress(self, name, host, server_host, port):
+        self.wfile.write('Within setup ingress...')
         template_data = """---
         apiVersion: extensions/v1beta1
         kind: Ingress
@@ -66,10 +67,12 @@ class S(BaseHTTPRequestHandler):
             host = item['ingress_host']
             server_host = item['server_host']
             port = item['port']
+            self.wfile.write('Values obtained : name: '+name+' host:'+ingress_host+' server_host: '+server_host+' port: '+port)
             #arguments = ['/opt/bin/getResponse.sh','%s' % jiratask,'%s' % changenumber,'%s' % changedate,'%s' % changestarttime,'%s' % changeendtime,'%s' % changeimplementer,'%s' % environment,'%s' % servers,'%s' % project,'%s' %interfaceid,'%s' % release,'%s' % username,'%s' % password,'%s' % changeSysId]
             #p = Popen(arguments, stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=-1)
             try:
-                _setup_ingress(name, host, server_host, port)
+                self.wfile.write('Setting up ingress...')
+                _setup_ingress(self, name, host, server_host, port)
                 #output, error = p.communicate()
 
                 #if p.returncode > 1:
